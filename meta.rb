a0e7a36c16f03b9aaa67d552c3ac85718edae42c
voodoologic <headley.douglas@gmail.com>
2016-06-28 15:36:47 -0700

    String for offset_date

--- meta.rb
+++ meta.rb
@@ -5,7 +5,7 @@ module DB
     property :project, String
     property :start_time, DateTime
     property :end_time, DateTime
-    property :offset_date, Integer, default: nil
+    property :offset_date, String
     property :updated_count, Integer, default: 0
     property :restart_count, Integer, default: 0
   end
